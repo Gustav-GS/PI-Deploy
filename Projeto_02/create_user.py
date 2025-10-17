18c23850ed7f4283768223d243f272fa57e07d61
Gustavo <98180025+Gustav-GS@users.noreply.github.com>
2025-10-16 22:35:50 -0300

    remove unused files

--- Projeto_02/create_user.py
+++
@@ -1,9 +0,0 @@
-from app import app, db, User
-
-with app.app_context():
-    db.create_all()
-    new_user = User(username='rodrigo', password='23207360')  # Em produção, use hash!
-    db.session.add(new_user)
-    db.session.commit()
-
-    print("Usuário criado com sucesso!")
