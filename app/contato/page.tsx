99edef22556913a2c3379e1929444ef3616489e7
Gustavo <98180025+Gustav-GS@users.noreply.github.com>
2025-10-28 22:02:36 -0300

    fix: adjust phone number

--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -6,7 +6,7 @@ export default function ContatoPage() {
       <h2>Contato</h2>
       <ul>
         <li>E-mail: contato@rotarybariri.org</li>
-        <li>Telefone: (14) 1234-5678</li>
+        <li>Telefone: (14) 99628-6798</li>
         <li>Endereço: Av. João Lemos, 311 - Centro <br/> Bariri/SP CEP: 17.257-000</li>
       </ul>
       <p><Link href="/">Voltar</Link></p>
